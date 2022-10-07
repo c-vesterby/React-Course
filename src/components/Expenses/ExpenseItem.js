e32df50ecd7c806add6a6ac9b2c5f38855ddd275
Christine Vesterby <christinevesterby@gmail.com>
2022-10-07 10:33:32 -0400

    Adds onClick option for title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,16 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Update!');
+    console.log(title);
+  };
  
   return (
     <Card className='expense-item'>
@@ -11,6 +19,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
